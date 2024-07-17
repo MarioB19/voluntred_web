f0e961e7bc0a6d9ec2f7d170f6ffd11fe5d44122
MarioM <a20300683@live.ceti.mx>
2024-07-16 19:33:21 -0600

    Correcion de responsividad de dispositivos en cuadricula ODS

--- src/components/ODSCard.js
+++ src/components/ODSCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { styled } from '@mui/system';
+import { Box, Typography, useMediaQuery } from '@mui/material';
+import { styled, useTheme } from '@mui/system';
 
 const Card = styled(Box)(({ theme }) => ({
   perspective: '1000px',
+  width: '100%',
   '&:hover .inner': {
     transform: 'rotateY(180deg)',
   },
@@ -11,8 +12,8 @@ const Card = styled(Box)(({ theme }) => ({
 
 const Inner = styled(Box)(({ theme, color }) => ({
   position: 'relative',
-  width: '300px',  // Ajustamos el ancho a 300px
-  height: '300px',  // Ajustamos la altura a 300px para hacerla cuadrada
+  width: '100%',
+  paddingTop: '100%', // Esto mantiene la relación de aspecto 1:1
   textAlign: 'center',
   transition: 'transform 0.6s',
   transformStyle: 'preserve-3d',
@@ -21,66 +22,66 @@ const Inner = styled(Box)(({ theme, color }) => ({
   backgroundColor: color,
 }));
 
-const Front = styled(Box)(({ theme, image }) => ({
+const Face = styled(Box)({
   position: 'absolute',
+  top: 0,
+  left: 0,
   width: '100%',
   height: '100%',
   backfaceVisibility: 'hidden',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  fontSize: '2rem',
-  color: '#fff',
   borderRadius: '8px',
+});
+
+const Front = styled(Face)(({ image }) => ({
   backgroundImage: `url(${image})`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
 }));
 
-const Back = styled(Box)(({ theme, color }) => ({
-  position: 'absolute',
-  width: '100%',
-  height: '100%',
-  backfaceVisibility: 'hidden',
-  display: 'flex',
+const Back = styled(Face)(({ color }) => ({
   flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
   color: '#fff',
   transform: 'rotateY(180deg)',
-  borderRadius: '8px',
-  padding: theme.spacing(2),
+  padding: '8px',
   backgroundColor: color,
 }));
 
-const Title = styled(Typography)(({ theme }) => ({
-  fontSize: '1.5rem',  // Título más grande
+const Title = styled((props) => <Typography {...props} />)(({ theme, isMobile }) => ({
+  fontSize: isMobile ? '1rem' : '2rem',
   fontWeight: 'bold',
   textAlign: 'center',
 }));
 
-const Description = styled(Typography)(({ theme }) => ({
-  fontSize: '1.1rem',
+const Description = styled((props) => <Typography {...props} />)(({ theme, isMobile }) => ({
+  fontSize: isMobile ? '0.6rem' : '1.5rem',
   textAlign: 'center',
   '& span': {
-    fontWeight: 'bold',  // Palabra "Objetivo" en negritas
+    fontWeight: 'bold',
   },
 }));
 
-const ODSCard = ({ color, image, title, description }) => (
-  <Card>
-    <Inner className="inner" color={color}>
-      <Front image={image} />
-      <Back color={color}>
-        <Title variant="h6" component="div">
-          {title}
-        </Title>
-        <Description variant="body1" sx={{ fontSize: '1.1rem', textAlign: 'center' }}>
-          <span>Objetivo:</span> {description}
-        </Description>
-      </Back>
-    </Inner>
-  </Card>
-);
+const ODSCard = ({ color, image, title, description }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Card>
+      <Inner className="inner" color={color}>
+        <Front image={image} />
+        <Back color={color}>
+          <Title isMobile={isMobile} variant="h6" component="div">
+            {title}
+          </Title>
+          <Description isMobile={isMobile} variant="body2">
+            <span>Objetivo:</span> {description}
+          </Description>
+        </Back>
+      </Inner>
+    </Card>
+  );
+};
 
 export default ODSCard;
